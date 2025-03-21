406770ef2bb8f38caf360fd7b9a291e99112416f
bbq-bean <github.com/bbq-bean>
2025-03-21 16:55:09 -0500

    2sum II

--- 2sumSorted.py
+++ 2sumSorted.py
@@ -1,24 +1,5 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: Int):
-# from a list of numbers, return 2
-# list is in ascending order
-# probably 2 pointers is better
-
-# brute force
-# l = 0
-#
-# while l < len(numbers):
-# for r in range(l + 1, len(numb
-# if numbers[l] + numbers[r]
-# return [l+1, r+1]
-#
-#
-# l += 1
-# O(n^2)
-# O(1) space
-
-# 2 pointers solution
-# logic:
 # numbers are sorted, and we KNOW ther
 # so if current sum is less, get a big
 # if its more, try a smaller number
